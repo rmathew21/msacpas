ceedfc7bc419c23106fe4d5aa62131485cbb1475
rob <robmathew21@gmail.com>
2026-06-03 09:10:40 -0500

    fix:updated formspree endpoint

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -42,7 +42,7 @@ export default function Contact() {
     setError('');
 
     try {
-      const res = await fetch('https://formspree.io/f/mlgvbllp', {
+      const res = await fetch('https://formspree.io/f/meewjggo', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
         body: JSON.stringify(form),
